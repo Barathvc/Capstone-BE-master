8a1d210552285e4439a0abb0fd86c171b40de410
Your Name <you@example.com>
2024-06-04 22:38:36 +0530

    5th

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 app.use(cors({
-    origin: "https://cleanease07.netlify.app",
+    origin: "https://cleanease-capstone-fe.netlify.app/",
     credentials: true
 }));
 app.use(express.json());
